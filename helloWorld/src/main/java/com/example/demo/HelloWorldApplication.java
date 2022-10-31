ee25029885657a17b78605a195efe64f7a6d65be
MalekMohanna <malekmhana@gmail.com>
2022-10-31 15:24:43 +0200

    first commit

--- helloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
+++ helloWorld/src/main/java/com/example/demo/HelloWorldApplication.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/Daikichi")
+@RequestMapping("/daikichi")
 public class HelloWorldApplication {
 	@RequestMapping("")
 	public String hello() {
@@ -25,6 +26,17 @@ public String today() {
 public String tomorrow() {
 	return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 }
-
+@RequestMapping("/travel/{where}")
+public String travel(@PathVariable("where") String where1) {
+	return "Congratulations! You will soon travel to "+where1+"!";
+}
+	
+@RequestMapping("/lotto/{num}")
+public String lotto(@PathVariable("num") int number) {
+	if(number%2==0) {
+		return "You will take a grand journey in the near future, but be weary of tempting offers'.";
+	}
+	return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+}
 }
 
