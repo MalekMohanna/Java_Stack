635eb1a7e8f874bfeaeda2aeb5f0a49a57243fdd
MalekMohanna <malekmhana@gmail.com>
2022-11-10 11:06:59 +0200

    first commit

---
+++ LoginRegis/src/main/java/com/malek/login/LoginRegisApplication.java
@@ -0,0 +1,13 @@
+package com.malek.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegisApplication.class, args);
+	}
+
+}
